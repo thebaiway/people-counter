bb78c21ab45b192e7d9a1a3311caf54365d6abd1
Bailee <bailee.roby@gmail.com>
2023-01-25 22:48:04 -0800

    finishing up logic and done converting

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
 import './App.css';
 import background from './Images/istockphoto-1255056515-612x612.jpg'
+import React, {useState} from 'react'
 
 function App() {
 
-// let countEl = document.getElementByClassName("count-el")
-// let saveEl = document.getElementByClassName("save-el")
+  const [count, setCount] = useState(0)
 
-// let count = 0
+  function add () {
+    setCount(prevCount => prevCount + 1)
+  }
 
-// function increment () {
-//     count += 1
-//     countEl.textContent = count
-// }
+  const [save, setSave] = useState("")
 
-// function save () {
-//     let countStr = count + " - "
-//     saveEl.textContent += countStr
-//     console.log(count)
-//     count = 0
-//     countEl.textContent = 0
-// }
+  function saveEntry () {
+    setSave(prevSave => prevSave += count + " - ")
+  }
 
-// function reset () {
-//   count = 0
-//   countEl.textContent = 0
-//   saveEl.textContent = "Previous entries: "
-// }
+  function resetEntry () {
+    setCount(prevCount => prevCount = 0)
+    setSave(prevSave => prevSave = " ")
+  }
 
   return (
 
     <div className="container" style = {{backgroundImage: `url(${background})`}}>
         <h1>People Entered:</h1>
-        <h2 className="count-el">0</h2>
-        <button className="increment-btn" onclick="increment()">INCREMENT</button>
-        <button className="save-btn" onclick="save()">SAVE</button>
-        <p className ="save-el">Previous Entries: </p>
-        <button className ="reset-btn" onclick="reset()">RESET ENTRIES</button>
+        <h2 className="count-el">{count}</h2>
+        <button className="increment-btn" onClick = {add}>INCREMENT</button>
+        <button className="save-btn" onClick = {saveEntry}>SAVE</button>
+        <p className ="save-el">Previous Entries: {save} </p>
+        <button className ="reset-btn" onClick = {resetEntry}>RESET ENTRIES</button>
     </div>
-  );
+  )
 }
 
 export default App;
